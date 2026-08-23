37be918c04da5dff9505e32dea616f0729871538
Turkista <turkista.com.br@gmail.com>
2026-08-23 20:25:28 -0300

    Delete abrir-painel.bat
    esse arquivo é bat

--- abrir-painel.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0painel-turkfitness"
-if not exist node_modules (
-  echo Instalando dependencias do painel pela primeira vez...
-  call npm install
-)
-start http://localhost:3000
-call npm start
