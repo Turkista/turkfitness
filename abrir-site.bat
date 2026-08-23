1994086316f90aa136abddfe0478cd716ad68adf
Turkista <turkista.com.br@gmail.com>
2026-08-23 20:26:13 -0300

    Delete abrir-site.bat
    esse arquivo é bat

--- abrir-site.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Turk Fitness - Preview do site
-cd /d "%~dp0"
-echo.
-echo Abrindo o site Turk Fitness...
-echo (esta janela precisa ficar aberta enquanto voce navega no site)
-echo.
-node preview-site.js
-pause
